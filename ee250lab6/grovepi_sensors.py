08b95659ffe114fa755db37f00f49fa6ee502b8d
Prital <prital.jariwala@gmail.com>
2025-10-19 20:27:11 -0700

    edited

--- ee250lab6/grovepi_sensors.py
+++ ee250lab6/grovepi_sensors.py
@@ -10,27 +10,31 @@ ultrasonic_ranger = 2
 potentiometer = 0
 grovepi.pinMode(potentiometer,"INPUT")
 
-# clear lcd screen  before starting main loop
-setText("")
+grovepi.lcdInit()
 
+# clear lcd screen  before starting main loop
+grovepi.setText("")
+
+
+# TODO:read distance value from Ultrasonic Ranger and print distance on LCD
 while True:
   try:
-    # TODO:read distance value from Ultrasonic Ranger and print distance on LCD
-    while True:
-    try:
-        # Read distance value from Ultrasonic
-        print(grovepi.ultrasonicRead(ultrasonic_ranger))
+    # Read distance value from Ultrasonic
+    ultrasonic_reading = grovepi.ultrasonicRead(ultrasonic_ranger)
+    threshold = grovepi.analogRead(potentiometer)
+    print(f"Distance: {ultrasonic_reading}")
 
-    except Exception as e:
-        print ("Error:{}".format(e))
-    
-    time.sleep(0.1)
-
-    # TODO: read threshold from potentiometer
-
-    
-    # TODO: format LCD text according to threshhold
+    if ultrasonic_reading < threshold:
+      obj_present = "OBJ PRES"
+    else:
+      obj_present = ""
   
+  except Exception as e:
+    print ("Error:{}".format(e))
+  time.sleep(0.1)
+        
     
+  # TODO: format LCD text according to threshhold
+  grovepi.setText()
   except IOError:
     print("Error")
